e9f5a8254c609a52a129bbfa8c9dd255cbd9d556
Daniel Medina-Neira <danielmedinaneira555@gmail.com>
2022-05-03 07:53:18 -0500

    Include the generation of the game in the Shiny app

--- README.md
+++ README.md
@@ -2,9 +2,4 @@
 
 This repo contains code to make some parts of the game Secret Hitler easier.
 
-## Dynamic
-
-There are two parts to create a game using the code:
-
-1. Generate a game: use the function `create_game()`.
-2. Get the roles for each player: use the app `app.R`.
+The code can be used through the Shiny app, which has two parts. One for generating the code of the game and the IDs of the players and the other to obtain the roles for each of the players using the code of the game.
--- app.R
+++ app.R
@@ -5,6 +5,15 @@ library(purrr)
 walk(list.files("www", full.names = TRUE, pattern = "[.]R$"), source)
 
 ui <- fluidPage(
+  
+  # Create game
+  numericInput("n_players", "Number of players", value = 5, min = 5, max = 10),
+  actionButton("create_game", "Create game"),
+  verbatimTextOutput("game_code"),
+  
+  HTML("<hr>"),
+  
+  # Get roles
   textInput("code", "Code of the game"),
   textInput("id", "ID of the player"),
   actionButton("get_info", "Get role and information"),
@@ -12,6 +21,13 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
+  
+  # Create game
+  observeEvent(input$create_game, {
+    output$game_code <- renderPrint(create_game(input$n_players))
+  })
+  
+  # Get roles
   role <- eventReactive(input$get_info, get_role(input$code, input$id))
   output$result <- renderPrint(role())
 }
