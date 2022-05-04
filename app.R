f78eab5ff730ba99a3ab4c4e2006e5494c088be6
Daniel Medina-Neira <danielmedinaneira555@gmail.com>
2022-05-04 09:53:35 -0500

    Fix the order of the tabs and add description

--- app.R
+++ app.R
@@ -8,23 +8,31 @@ ui <- fluidPage(
   
   titlePanel("Secret Hitler"),
   
+  HTML(paste(
+    "This is a web app designed by DaniMedi. The instructions and other",
+    "details of the app are available on the <a href=''>DaniMedi website</a>,",
+    "and the code is available on",
+    "<a href='https://github.com/danimedi/secret_hitler_game'>GitHub</a>.",
+    "<br><br>"
+  )),
+  
   tabsetPanel(
     
     tabPanel(
       title = "Get roles",
       
-      numericInput("n_players", "Number of players", value = 5, min = 5, max = 10),
-      actionButton("create_game", "Create game"),
-      verbatimTextOutput("game_code")
+      textInput("code", "Code of the game"),
+      textInput("id", "ID of the player"),
+      actionButton("get_info", "Get role and information"),
+      verbatimTextOutput("result")
     ),
     
     tabPanel(
       title = "Create game",
       
-      textInput("code", "Code of the game"),
-      textInput("id", "ID of the player"),
-      actionButton("get_info", "Get role and information"),
-      verbatimTextOutput("result")
+      numericInput("n_players", "Number of players", value = 5, min = 5, max = 10),
+      actionButton("create_game", "Create game"),
+      verbatimTextOutput("game_code")
     )
     
   )
@@ -34,7 +42,7 @@ server <- function(input, output, session) {
   
   # Create game
   observeEvent(input$create_game, {
-    output$game_code <- renderPrint(create_game(input$n_players))
+    output$game_code <- renderPrint(isolate(create_game(input$n_players)))
   })
   
   # Get roles
